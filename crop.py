1c7be151bc0ce5e9aa207fe714df2d79d01a0313
Hemant Banke <hemantbanke5@gmail.com>
2018-08-30 19:07:23 +0530

    Crop and Save

--- crop.py
+++ crop.py
@@ -1,13 +1,31 @@
 
 import os
-import cv2
+from PIL import Image
 
 
-SOURCE_DIR = str( input("Enter image set source : ")) + "/"
+SOURCE_DIR = "/" + str( input("Enter image set source : ")) + "/"
 DEST_DIR = SOURCE_DIR + "crops/"
 
+CROP_AREA = (66, 124, 1904, 1026)
+
 #list of image filenames
-img = []
+for file_name in [ f for f in os.listdir(SOURCE_DIR) if os.path.isfile(SOURCE_DIR + f) ]:
+
+    #open images
+    try:
+        img = Image.open( SOURCE_DIR + file_name)
+    except OSError:
+        continue
+
+    assert ( img.size[0] >= CROP_AREA[2] and img.size[1] >= CROP_AREA[3]) , "Image is too small for defined crop area"
+
+    #crop
+    crop_img = img.crop(CROP_AREA)
+
+    if not os.path.exists( DEST_DIR ):
+        os.makedirs( DEST_DIR )
+
+    crop_img.save( DEST_DIR + file_name )
 
 
 
